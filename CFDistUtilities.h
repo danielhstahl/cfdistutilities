ac2c359bc6a99a9426193506bbddbf7ff9ea0cb9
Daniel Stahl <danstahl1138@gmail.com>
2018-03-06 19:52:11 -0600

    fixed move semantics

--- CFDistUtilities.h
+++ CFDistUtilities.h
@@ -46,7 +46,7 @@ namespace cfdistutilities {
 
         return -newton::bisect([&](const auto& pointInX){
 
-            return fangoost::computeExpectationPointDiscrete(pointInX, xMin, xMax, discreteCF, [&](const auto& u, const auto& x, const auto& index){
+            return fangoost::computeExpectationPointDiscrete(pointInX, xMin, xMax, std::move(discreteCF), [&](const auto& u, const auto& x, const auto& index){
                 return VkCDF(u, x, xMin, xMax, index);
             })-alpha;
         }, xMin, xMax, prec1, prec2);
@@ -54,25 +54,25 @@ namespace cfdistutilities {
 
     template<typename Number, typename CFDiscrete, typename Index>
     auto computeCDF(const Index& numXDiscrete, const Number& xMin, const Number& xMax, CFDiscrete&& discreteCF){
-        return fangoost::computeExpectationDiscrete(numXDiscrete, xMin, xMax, discreteCF, [&](const auto& u, const auto& x, const auto& index){
+        return fangoost::computeExpectationDiscrete(numXDiscrete, xMin, xMax, std::move(discreteCF), [&](const auto& u, const auto& x, const auto& index){
             return VkCDF(u, x, xMin, xMax, index);
         });
     }
     template<typename Number, typename CF, typename Index>
     auto computeCDF(const Index& numXDiscrete, const Index& numU, const Number& xMin, const Number& xMax, CF&& cf){
-        return fangoost::computeExpectation(numXDiscrete, numU, xMin, xMax, cf, [&](const auto& u, const auto& x, const auto& index){
+        return fangoost::computeExpectation(numXDiscrete, numU, xMin, xMax, std::move(cf), [&](const auto& u, const auto& x, const auto& index){
             return VkCDF(u, x, xMin, xMax, index);
         });
     }
     template<typename Number, typename CFDiscrete>
     auto computeCDFAtPoint(const Number& xValue, const Number& xMin, const Number&xMax, CFDiscrete&& cfDiscrete){
-        return fangoost::computeExpectationPointDiscrete(xValue, xMin, xMax, cfDiscrete, [&](const auto& u, const auto& x, const auto& index){
+        return fangoost::computeExpectationPointDiscrete(xValue, xMin, xMax, std::move(cfDiscrete), [&](const auto& u, const auto& x, const auto& index){
             return VkCDF(u, x, xMin, xMax, index);
         });
     }
     template<typename Number, typename CF, typename Index>
     auto computeCDFAtPoint(const Number& xValue,const Index& numU, const Number& xMin, const Number&xMax, CF&& cf){
-        return fangoost::computeExpectationPoint(xValue, xMin, xMax, numU, cf, [&](const auto& u, const auto& x, const auto& index){
+        return fangoost::computeExpectationPoint(xValue, xMin, xMax, numU, std::move(cf), [&](const auto& u, const auto& x, const auto& index){
             return VkCDF(u, x, xMin, xMax, index);
         });
     }
@@ -81,7 +81,7 @@ namespace cfdistutilities {
     template<typename Number, typename CFDiscrete>
     auto computeVaRNewtonHelper(const Number& alpha, const Number& xMin, const Number& xMax, const Number& guess, CFDiscrete&& discreteCF, const Number& prec1, const Number& prec2){
         return -newton::zeros([&](const auto& pointInX){
-            return fangoost::computeExpectationPointDiscrete(pointInX, xMin, xMax, discreteCF, [&](const auto& u, const auto& x, const auto& index){
+            return fangoost::computeExpectationPointDiscrete(pointInX, xMin, xMax, std::move(discreteCF), [&](const auto& u, const auto& x, const auto& index){
                 return VkCDF(u, x, xMin, xMax, index);
             })-alpha;
         }, guess, prec1, prec2, 50);
@@ -89,22 +89,22 @@ namespace cfdistutilities {
 
     template<typename Number, typename CF, typename Index>
     auto computeVaR(const Number& alpha, const Number& prec, const Number& xMin, const Number& xMax, const Index& numU, CF&& cf){
-        return computeVaRHelper(alpha, xMin, xMax, fangoost::computeDiscreteCFReal(xMin, xMax, numU, cf), prec, prec);
+        return computeVaRHelper(alpha, xMin, xMax, fangoost::computeDiscreteCFReal(xMin, xMax, numU, std::move(cf)), prec, prec);
     }
 
     template<typename Number, typename CF, typename Index>
     auto computeVaRNewton(const Number& alpha, const Number& prec1, const Number& prec2, const Number& xMin, const Number& xMax, const Number& guess, const Index& numU, CF&& cf){
-        return computeVaRNewtonHelper(alpha, xMin, xMax, guess, fangoost::computeDiscreteCFReal(xMin, xMax, numU, cf), prec1, prec2);
+        return computeVaRNewtonHelper(alpha, xMin, xMax, guess, fangoost::computeDiscreteCFReal(xMin, xMax, numU, std::move(cf)), prec1, prec2);
     }
 
     template<typename Number, typename CFDiscrete>
     auto computeVaRNewtonDiscrete(const Number& alpha, const Number& prec1, const Number& prec2, const Number& xMin, const Number& xMax, const Number& guess, CFDiscrete&& cf){
-        return computeVaRNewtonHelper(alpha, xMin, xMax, guess,cf, prec1, prec2);
+        return computeVaRNewtonHelper(alpha, xMin, xMax, guess, std::move(cf), prec1, prec2);
     }
 
     template<typename Number, typename CFDiscrete>
     auto computeVaRDiscrete(const Number& alpha, const Number& prec, const Number& xMin, const Number& xMax, CFDiscrete&& cf){
-        return computeVaRHelper(alpha, xMin, xMax, cf, prec, prec);
+        return computeVaRHelper(alpha, xMin, xMax, std::move(cf), prec, prec);
     }
 
 
@@ -117,7 +117,7 @@ namespace cfdistutilities {
             ),
             xMin, 
             xMax,
-            cf, 
+            std::move(cf), 
             [&](const auto& u, const auto& x, const auto& index){
                 return VkE(u, x, xMin, xMax, index);
             }
@@ -129,7 +129,7 @@ namespace cfdistutilities {
             -VaR,
             xMin, 
             xMax,
-            cf, 
+            std::move(cf), 
             [&](const auto& u, const auto& x, const auto& index){
                 return VkE(u, x, xMin, xMax, index);
             }
@@ -141,26 +141,26 @@ namespace cfdistutilities {
     template<typename Number, typename CF, typename Index>
     auto computeES(const Number& alpha, const Number& prec, const Number& xMin, const Number& xMax, const Index& numU, CF&& cf){
         return computeESDiscrete(alpha, prec, xMin, xMax, 
-            fangoost::computeDiscreteCFReal(xMin, xMax, numU, cf)
+            fangoost::computeDiscreteCFReal(xMin, xMax, numU, std::move(cf))
         );
     }
     template<typename Number, typename CF, typename Index>
     auto computeES(const Number& alpha, const Number& xMin, const Number& xMax, const Number& VaR, const Index& numU, CF&& cf, bool includeVaR){
         return computeESDiscrete(alpha, xMin, xMax, VaR,
-            fangoost::computeDiscreteCFReal(xMin, xMax, numU, cf),
+            fangoost::computeDiscreteCFReal(xMin, xMax, numU, std::move(cf)),
             includeVaR
         );
     }
     
     template<typename Number, typename CF, typename Index>
     auto computeEL(const Number& xMin, const Number& xMax, const Index& numU, CF&& cf){
-        return fangoost::computeExpectationPoint(xMax, xMin, xMax, numU, cf, [&](const auto& u, const auto& x, const auto& index){
+        return fangoost::computeExpectationPoint(xMax, xMin, xMax, numU, std::move(cf), [&](const auto& u, const auto& x, const auto& index){
             return VkE(u, x, xMin, xMax, index);
         });
     }
     template<typename Number, typename CFDiscrete>
     auto computeELDiscrete(const Number& xMin, const Number& xMax, CFDiscrete&& cf){
-        return fangoost::computeExpectationPointDiscrete(xMax, xMin, xMax, cf, [&](const auto& u, const auto& x, const auto& index){
+        return fangoost::computeExpectationPointDiscrete(xMax, xMin, xMax, std::move(cf), [&](const auto& u, const auto& x, const auto& index){
             return VkE(u, x, xMin, xMax, index);
         });
     }
